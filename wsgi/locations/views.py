0a9d2baf3c555c221657c77f5cca757af74f4af3
kwiat78 <kwiat78@gmail.com>
2016-04-26 23:49:32 +0200

    Add SnapAviView prototype.

--- wsgi/locations/views.py
+++ wsgi/locations/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
+from rest_framework.decorators import detail_route
 from rest_framework.renderers import JSONRenderer
+import requests
 
 from locations.models import Location
 from locations.serializers import LocationSerializer
@@ -21,4 +23,33 @@ class TrackApiView(APIView):
     def get(self,request,label=None):
         if label:
             return Response(map(lambda x:{'longitude':x.longitude, 'latitude':x.latitude}, Location.objects.filter(label=label)))
+
+
         return Response(map(lambda x:x["label"], Location.objects.values("label").distinct()))
+
+
+    def post(self, request, label):
+        new_label = request.data.get("new_label", None)
+        if not new_label:
+            return Response("Specify new_label", status=403)
+        Location.objects.filter(label=label).update(label=new_label)
+        return Response(status=200)
+
+
+class SnapApiView(APIView):
+    def get(self,request,label):
+        print("XXXXXXXXXXXXXXXXX")
+        url = 'https://roads.googleapis.com/v1/snapToRoads'
+        key ="AIzaSyBir6gtAnK2Ck9Te9ibcTbnO9SQKdQPBNg"
+        interpolate= True
+        path = "|".join(list(map(lambda x:str(x.longitude)+","+str(x.latitude), Location.objects.filter(label=label))))
+        print(label)
+        print(path)
+
+        response = requests.get(url=url,params={"key":key, "interpolate":interpolate,"path":path})
+        return Response(response.json())
+
+
+
+
+
