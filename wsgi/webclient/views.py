3944586df5be8b559156576400916e877f3ea9da
kwiat78 <kwiat78@gmail.com>
2016-05-08 22:43:40 +0200

    Fix templates.

--- wsgi/webclient/views.py
+++ wsgi/webclient/views.py
@@ -1,6 +1,9 @@
  
-from django.template import RequestContext
-from django.shortcuts import render
+# from django.template import RequestContext
+# from django.shortcuts import render
+from django.template import Context
+from django.template.loader import get_template
+from django.template.response import TemplateResponse
 
 def index(request):
     # Request the context of the request.
@@ -14,4 +17,7 @@ def index(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request,'./index.html')
+    template = get_template('index.html')
+    #return template.render({},request=request)
+    return TemplateResponse(request,template)
+    #return render(request,'./index.html')
