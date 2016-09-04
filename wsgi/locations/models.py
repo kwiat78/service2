94677645445941a6dd58a623c0389c866219d324
kwiat78 <kwiat78@gmail.com>
2016-09-04 12:38:56 +0200

    Add Track model and add track ForeignKey to Location.

--- wsgi/locations/models.py
+++ wsgi/locations/models.py
@@ -2,14 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Track(models.Model):
+    user = models.ForeignKey(User)
+    label = models.CharField(max_length=256)
+    procesed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{}-{}".format(self.user.username, self.label)
+
+
 class Location(models.Model):
     user = models.ForeignKey(User)
     latitude = models.FloatField(default=0)
     longitude = models.FloatField(default=0)
     date = models.DateTimeField()
     label = models.CharField(max_length=256)
+    track = models.ForeignKey(Track, null=True)
 
     def __str__(self):
         return "{}-{}-({},{})".format(self.user.username, self.label,
                                       self.latitude, self.longitude)
-
